3b44a4fd5351dcdcc69513bf0f8e40152739e7de
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2025-09-16 17:55:02 +0300

    feat: add solution 13/56

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -177,8 +177,9 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const order = 10 ** pow;
+  return Math.round(num / order) * order;
 }
 
 /**
@@ -198,8 +199,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let result = true;
+  for (let i = 2; i < n; i += 1) {
+    if (n > 3 && n % i === 0) {
+      result = false;
+    }
+  }
+  return result;
 }
 
 /**
